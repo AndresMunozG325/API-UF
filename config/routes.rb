841a2e456c7fe78c82c07c42a94a36f442689640
Andres <andresmunozgo@gmail.com>
2021-01-20 14:04:48 -0300

    define routes in routes file to access methods in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :clients
+
+  get "/uf/:date", to: 'clients#consult_uf'
+  get "/client/:name", to: 'clients#my_queries'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
